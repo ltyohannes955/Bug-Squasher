af4b580f9d8165e9595536e64a5b7b7b19686f9c
tito yohannes <ltyohannes95@gmail.com>
2023-04-06 11:14:45 +0300

    Profile page_UI

--- lib/views/profile.dart
+++ lib/views/profile.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
 
 class profile extends StatefulWidget {
   const profile({super.key});
@@ -11,6 +11,121 @@ class profile extends StatefulWidget {
 class _profileState extends State<profile> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+      child: Scaffold(
+        // appBar: AppBar(
+          
+        //   actions: [
+            
+        //     IconButton(onPressed: (){}, icon: Icon(Icons.qr_code))
+        //   ],
+        // ),
+        body: Column(
+          children: [
+            
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: IconButton(onPressed: (){}, icon: Icon(Icons.arrow_back, color: Colors.purple, size: MediaQuery.of(context).size.width*.045,),),
+                )
+                
+                
+              ],
+            ),
+            Center(
+              child: Text(
+                'Business cards',
+                style: TextStyle(fontWeight: FontWeight.bold, color: Colors.purple, fontSize: MediaQuery.of(context).size.height*.055),)
+              ),
+
+              Card(
+                elevation: 20,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20)
+                  ),
+                  height: MediaQuery.of(context).size.height*.3,
+                  width: MediaQuery.of(context).size.width*.9,
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          Icon(Icons.person,
+                          size: MediaQuery.of(context).size.height*.2,),
+                        ],
+                      ),
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('Abebe Kebede', style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width*.055),),
+                          RichText(text: 
+                          const TextSpan(
+                            text: 'Work Area: ', style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black),
+                            children: <TextSpan>[
+                              TextSpan(text: 'Engineering', style: TextStyle(color: Colors.grey))
+                            ]
+                          )),
+                          RichText(text: 
+                          const TextSpan(
+                            text: 'Email: ', style: TextStyle(fontWeight: FontWeight.bold, ),
+                            children: <TextSpan>[
+                              TextSpan(text: 'test@gmail.com', style: TextStyle(color: Colors.grey))
+                            ]
+                          )),
+                          RichText(text: 
+                          const TextSpan(
+                            text: 'Phone NO: ', style: TextStyle(fontWeight: FontWeight.bold, ),
+                            children: <TextSpan>[
+                              TextSpan(text: '0912345678', style: TextStyle(color: Colors.grey))
+                            ]
+                          )),
+                          RichText(text: 
+                          const TextSpan(
+                            text: 'Job Type: ', style: TextStyle(fontWeight: FontWeight.bold, ),
+                            children: <TextSpan>[
+                              TextSpan(text: 'CEO', style: TextStyle(color: Colors.grey))
+                            ]
+                          )),
+                          RichText(text: 
+                          const TextSpan(
+                            text: 'company: ', style: TextStyle(fontWeight: FontWeight.bold, ),
+                            children: <TextSpan>[
+                              TextSpan(text: 'ABC .PLC', style: TextStyle(color: Colors.grey))
+                            ]
+                          )),
+                          Row(
+                            children: [
+                              ElevatedButton(onPressed: (){
+                                Navigator.pushNamed(context, "");
+                              },
+                              style: ElevatedButton.styleFrom(
+                                primary: Colors.purple,
+                                
+                              )
+                              , child: Text("EDIT", style: TextStyle(fontSize: MediaQuery.of(context).size.width*.055),) ),
+                              SizedBox(width: 10,),
+
+                              IconButton(onPressed: (){
+                                Navigator.pushNamed(context, '/setup');
+                              }, icon: Icon(Icons.qr_code, color: Colors.purple,size: MediaQuery.of(context).size.width*.075,))
+                              
+                            ],
+                          )
+                          
+                        ],
+                      )
+                      
+                    ],
+                  ),
+                ),
+              )
+
+          ],
+        ),
+      ),
+    );
   }
 }
