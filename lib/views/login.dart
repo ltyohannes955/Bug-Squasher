71be18a83d48b28c7e2dff53dadc01a396383fe8
Dagmawit.T <Dagitesh2188@gmail.com>
2023-04-06 14:18:24 +0300

    login and signup page UI

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -1,16 +1,83 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class login extends StatefulWidget {
-  const login({super.key});
+class loginScreen extends StatefulWidget {
+  const loginScreen({super.key});
 
   @override
-  State<login> createState() => _loginState();
+  State<loginScreen> createState() => _loginScreenState();
 }
 
-class _loginState extends State<login> {
+class _loginScreenState extends State<loginScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Log In",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: MediaQuery.of(context).size.height * .055,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .055,
+            ),
+            const TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  hintText: "User Email",
+                  prefixIcon: Icon(
+                    Icons.mail,
+                    color: Colors.black,
+                  )),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .055,
+            ),
+            const TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: Icon(
+                    Icons.lock,
+                    color: Colors.black,
+                  )),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .055,
+            ),
+            Container(
+              width: double.infinity,
+              child: RawMaterialButton(
+                fillColor: Color.fromARGB(248, 10, 17, 121),
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12.0)),
+                onPressed: () {},
+                child: const Text(
+                  "Login",
+                  style: TextStyle(color: Colors.white, fontSize: 18.0),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .055,
+            ),
+            TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/signup');
+                },
+                child: Text("Don't have an Account?Sign Up"))
+          ],
+        ),
+      ),
+    );
   }
 }
