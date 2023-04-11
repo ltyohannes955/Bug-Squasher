8505a8ec44c87fe69ff838f266ae6d688d64ad09
yitethio <yitbaandu703@gmail.com>
2023-04-11 15:15:53 +0300

    corrected some minor ui issu

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/': (context) => intro(),
+        '/': (context) => Category(),
         '/login': (context) => login(),
         '/signUp': (context) => signUp(),
         '/setup': (context) => setup(),
--- lib/views/category.dart
+++ lib/views/category.dart
@@ -7,56 +7,39 @@ class Category extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
+      right: true,
+      left: true,
       child: Scaffold(
-        backgroundColor: Color(0xff10455B),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(248, 10, 17, 121),
+          title: const Text(
+            'DBC',
+            style: TextStyle(
+                fontSize: 31.09,
+                color: Color.fromARGB(255, 255, 255, 255),
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.normal),
+          ),
+          centerTitle: true,
+          actions: [
+            Container(
+              child: IconButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/profile');
+                },
+                icon: const Icon(
+                  Icons.account_circle_outlined,
+                  size: 33,
+                ),
+              ),
+            )
+          ],
+        ),
         body: Column(children: [
           Expanded(
-              flex: 25,
+              flex: 5,
               child: Column(
                 children: [
-                  Container(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(320, 3, 0, 0),
-                      child: IconButton(
-                        color: Colors.white,
-                        onPressed: () {
-                          Navigator.pushNamed(context, '/profile');
-                        },
-                        icon: const Icon(
-                          Icons.account_circle_outlined,
-                          size: 39,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 250, 0),
-                        child: Container(
-                            height: MediaQuery.of(context).size.height * .1,
-                            width: MediaQuery.of(context).size.width * .5,
-                            child: Image(
-                                fit: BoxFit.cover,
-                                image: AssetImage(
-                                  'assets/images/logo.png',
-                                ))),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 0, 250, 0),
-                        child: Container(
-                          child: Text(
-                            'DBC',
-                            style: TextStyle(
-                                fontSize: 31.09,
-                                color: Color.fromARGB(255, 255, 255, 255),
-                                fontWeight: FontWeight.w300,
-                                fontStyle: FontStyle.normal),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
                   Padding(
                     padding: const EdgeInsets.fromLTRB(0, 10, 210, 0),
                     child: Container(
@@ -64,8 +47,8 @@ class Category extends StatelessWidget {
                         'Catagory',
                         style: TextStyle(
                             fontSize: 20.08,
-                            color: Color.fromARGB(255, 255, 255, 255),
-                            fontWeight: FontWeight.w300,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w400,
                             fontStyle: FontStyle.normal),
                       ),
                     ),
@@ -82,14 +65,25 @@ class Category extends StatelessWidget {
                     mainAxisSpacing: 15),
                 itemCount: 23,
                 itemBuilder: (BuildContext ctx, index) {
-                  return Container(
-                    height: 400,
-                    width: 137.11,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        color: Color.fromARGB(255, 134, 134, 131),
-                        borderRadius: BorderRadius.circular(15)),
-                    child: Text('hello'),
+                  return Padding(
+                    padding: const EdgeInsets.all(9.0),
+                    child: Container(
+                      height: 400,
+                      width: 137.11,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Color.fromARGB(255, 179, 173, 173),
+                              blurRadius: 17.0,
+                              spreadRadius: 5,
+                              offset: Offset(3, 3),
+                            ),
+                          ],
+                          color: Color.fromARGB(255, 124, 124, 122),
+                          borderRadius: BorderRadius.circular(15)),
+                      child: Text('hello'),
+                    ),
                   );
                 }),
           ),
