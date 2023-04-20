d83d090f3a564be5e96d04f56411424513fc578a
tito yohannes <ltyohannes95@gmail.com>
2023-04-20 06:28:03 +0300

    added fetch feature for edit_profile page

--- lib/views/edit_card.dart
+++ lib/views/edit_card.dart
@@ -1,7 +1,10 @@
+import 'package:digital_business_card/bloc/activity_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../database/service.dart';
 import 'constant/colors.dart';
 
 class edit_card extends StatefulWidget {
@@ -12,7 +15,17 @@ class edit_card extends StatefulWidget {
 }
 
 class _edit_cardState extends State<edit_card> {
-  int _val = 1;
+
+
+  @override
+  void initstate() {
+    super.initState();
+    setState(() {});
+  }
+
+  
+  
+  final _service = Services();
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -37,184 +50,201 @@ class _edit_cardState extends State<edit_card> {
 
             Text('EDIT PROFILE', style: TextStyle(fontWeight: FontWeight.bold, color: blue, fontSize: MediaQuery.of(context).size.width*.075),),
 
-            Container(
-              width: MediaQuery.of(context).size.width*.9,
-              height: MediaQuery.of(context).size.height*.8,
-              color: Colors.white,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(10,10,0,0),
-                    child: Text('Full Name:', style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width*.035),),
-                  ), 
-                  TextFormField(
-                    initialValue: 'Abebe kebede',
-                    decoration: InputDecoration(
-                      labelText: "Full Name",
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(10,10,0,0),
-                    child: Text('Work Area', style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width*.035),),
-                  ),
+            BlocBuilder<ActivityBloc, ActivityState>
+            (builder: (context, state){
+              if(state is ActivityInitial){
+                BlocProvider.of<ActivityBloc>(context)
+                 .add(const GetActivity());
 
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                   children: [
-                     Expanded(
-                       child: RadioListTile(
-                        activeColor: blue,
-                          title: Text('Enggineering'),
-                            value: 1, 
-                            groupValue: _val,
-                            onChanged: (value){
-                              setState(() {
-                                _val = value!;
-                              });
-                            }
-                          ),
-                     ),
-                        Expanded(
-                          child: RadioListTile(
-                            activeColor: blue,
-                            title: Text('Economics'),
-                              value: 2, 
-                              groupValue: _val,
-                              onChanged: (value){
-                          setState(() {
-                            _val = value!;
-                          });
-                          }
-                         ),
-                        ),
-                    
-                   ],
-                 ),
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                   children: [
-                     Expanded(
-                       child: RadioListTile(
-                        activeColor: blue,
-                          title: Text('tech'),
-                            value: 3, 
-                            groupValue: _val,
-                            onChanged: (value){
-                              setState(() {
-                                _val = value!;
-                              });
-                               //selected value
-                            }
-                          ),
-                     ),
-                        Expanded(
-                          child: RadioListTile(
-                            activeColor: blue,
-                            title: Text('Medicaine'),
-                              value: 4, 
-                              groupValue: _val,
-                              onChanged: (value){
-                          setState(() {
-                            _val = value!;
-                          });
-                           //selected value
-                           }
-                          ),
-                        ),
-                    
-                   ],
-                 ),
-                 Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                   children: [
-                     Expanded(
-                       child: RadioListTile(
-                        activeColor: blue,
-                          title: Text('Psychology'),
-                            value: 5, 
-                            groupValue: _val,
-                            onChanged: (value){
-                              setState(() {
-                                _val = value!;
-                              });
-                               //selected value
-                            }
-                          ),
-                     ),
-                        Expanded(
-                          child: RadioListTile(
-                            activeColor: blue,
-                            title: Text('Law'),
-                              value: 6, 
-                              groupValue: _val,
-                              onChanged: (value){
-                          setState(() {
-                            _val = value!;
-                          });
-                        }
-                       ),
-                        ),
-                    
-                   ],
-                 ),
+                 return Container();
+              }
+              if (state is ActivityLoading){
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }else if (state is ActivityLoaded){
+                return Container(
+                  height: MediaQuery.of(context).size.height*.8,
+                  width: MediaQuery.of(context).size.width*.9,
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    itemCount: state.card.length,
+                    itemBuilder: (BuildContext context, int Index){
+                      final val = state.card[Index];
+                      String _val = val.workArea;
+                      final _FullController = TextEditingController(text: val.FullName);
+                      final _emailcontroller = TextEditingController(text: val.email);
+                      final _phoneNOAreaController = TextEditingController(text: val.phoneNO.toString());
+                      final _jobcontroller = TextEditingController(text: val.jobType);
+                      final _companycontroller = TextEditingController(text: val.company);
+                      return Container(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              TextFormField(
+                                // initialValue: val.FullName,
+                                controller: _FullController,
+                                decoration: const InputDecoration(
+                                  labelText: "Full Name",
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(10,10,0,0),
+                                child: Text('Work Area', style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width*.035),),
+                              ),
 
-                 Padding(
-                    padding: const EdgeInsets.fromLTRB(10,10,0,0),
-                    child: Text('Email:', style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width*.035),),
-                  ), 
-                  TextFormField(
-                    initialValue: 'test@gmail.com',
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                    ),
-                  ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Expanded(
+                                  child: RadioListTile(
+                                    activeColor: blue,
+                                      title: const Text('Enggineering'),
+                                        value: 'Enggineering', 
+                                        groupValue: _val,
+                                        onChanged: (value){
+                                          setState(() {
+                                            _val = value!;
+                                          });
+                                        }
+                                      ),
+                                ),
+                                    Expanded(
+                                      child: RadioListTile(
+                                        activeColor: blue,
+                                        title: const Text('Economics'),
+                                          value: 'Economics', 
+                                          groupValue: _val,
+                                          onChanged: (value){
+                                      setState(() {
+                                        _val = value!;
+                                      });
+                                      }
+                                    ),
+                                    ),
+                                
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Expanded(
+                                  child: RadioListTile(
+                                    activeColor: blue,
+                                      title: const Text('tech'),
+                                        value: 'tech', 
+                                        groupValue: _val,
+                                        onChanged: (value){
+                                          setState(() {
+                                            _val = value!;
+                                          });
+                                          //selected value
+                                        }
+                                      ),
+                                ),
+                                    Expanded(
+                                      child: RadioListTile(
+                                        activeColor: blue,
+                                        title: const Text('Medicaine'),
+                                          value: 'Medicaine', 
+                                          groupValue: _val,
+                                          onChanged: (value){
+                                      setState(() {
+                                        _val = value!;
+                                      });
+                                      //selected value
+                                      }
+                                      ),
+                                    ),
+                                
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Expanded(
+                                  child: RadioListTile(
+                                    activeColor: blue,
+                                      title: const Text('Psychology'),
+                                        value: 'Psychology', 
+                                        groupValue: _val,
+                                        onChanged: (value){
+                                          setState(() {
+                                            _val = value!;
+                                          });
+                                          //selected value
+                                        }
+                                      ),
+                                ),
+                                    Expanded(
+                                      child: RadioListTile(
+                                        activeColor: blue,
+                                        title: const Text('Law'),
+                                          value: 'Law', 
+                                          groupValue: _val,
+                                          onChanged: (value){
+                                      setState(() {
+                                        _val = value!;
+                                      });
+                                    }
+                                  ),
+                                    ),
+                                
+                              ],
+                            ),
 
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(10,10,0,0),
-                    child: Text('Phone NO:', style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width*.035),),
-                  ), 
-                  TextFormField(
-                    initialValue: '0912345678',
-                    decoration: InputDecoration(
-                      labelText: "Phone NO",
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(10,10,0,0),
-                    child: Text('Job Type:', style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width*.035),),
-                  ), 
-                  TextFormField(
-                    initialValue: 'CEO',
-                    decoration: InputDecoration(
-                      labelText: "Jobe Type",
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(10,10,0,0),
-                    child: Text('Company:', style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width*.035),),
-                  ), 
-                  TextFormField(
-                    initialValue: 'ABC .PLC',
-                    decoration: InputDecoration(
-                      labelText: "Company",
-                    ),
-                  ),
-                  Padding(
-                      padding: const EdgeInsets.only(top: 20,left: 280),
-                      child: ElevatedButton(onPressed: (){
-                        Navigator.pushNamed(context, '/profile');
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: blue,
-                        
-                      ), 
-                      child: Text("Submit", style: TextStyle(fontSize: MediaQuery.of(context).size.width*.035),)),
-                    ),
-                  
-              ],)
-            )
+                              TextFormField(
+                                // initialValue: val.email,
+                                controller: _emailcontroller,
+                                decoration: const InputDecoration(
+                                  labelText: "Email",
+                                ),
+                              ),
+
+                              TextFormField(
+                                // initialValue: val.phoneNO.toString(),
+                                controller: _phoneNOAreaController,
+                                decoration: const InputDecoration(
+                                  labelText: "Phone NO",
+                                ),
+                              ),
+                              TextFormField(
+                                // initialValue: val.jobType,
+                                controller: _jobcontroller,
+                                decoration: const InputDecoration(
+                                  labelText: "Jobe Type",
+                                ),
+                              ),
+                              TextFormField(
+                                // initialValue: val.company,
+                                controller: _companycontroller,
+                                decoration: const InputDecoration(
+                                  labelText: "Company",
+                                ),
+                              ),
+                              Padding(
+                                  padding: const EdgeInsets.only(top: 20,left: 280),
+                                  child: ElevatedButton(onPressed: (){
+                                    Navigator.pushNamed(context, '/profile');
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    primary: blue,
+                                    
+                                  ), 
+                                  child: Text("Submit", style: TextStyle(fontSize: MediaQuery.of(context).size.width*.035),)),
+                                ),
+                              
+                          ],)
+            );
+                    }),
+                );
+                
+              }
+              return Container();
+            }),
+
+            
           ],
         ),
       ),
