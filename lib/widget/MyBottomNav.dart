0ce29116a42ff34cccf0e0745ccfbd852368f289
Samuel Fita <samabera03@gmail.com>
2023-04-26 16:43:37 +0300

    almost done

--- lib/widget/MyBottomNav.dart
+++ lib/widget/MyBottomNav.dart
@@ -1,3 +1,5 @@
+import 'dart:isolate';
+
 import 'package:digital_business_card/utils/rive_utils.dart';
 import 'package:digital_business_card/views/constant/colors.dart';
 import 'package:flutter/material.dart';
@@ -13,39 +15,65 @@ class MyBottomNav extends StatefulWidget {
 }
 
 class _MyBottomNavState extends State<MyBottomNav> {
-  late SMIBool searchTigger;
+  RiveAsset selectedBottomNav = bottomNavs.first;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: SafeArea(
         child: Container(
-          padding: EdgeInsets.all(12),
-          margin: EdgeInsets.symmetric(horizontal: 24),
+          padding: const EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 24),
           decoration: BoxDecoration(
             color: "#2B5B80".toColor(),
-            borderRadius: BorderRadius.all(Radius.circular(24)),
+            borderRadius: const BorderRadius.all(Radius.circular(24)),
           ),
           child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              GestureDetector(
-                onTap: () {
-                  searchTigger.change(true);
-                },
-                child: SizedBox(
-                  height: 36,
-                  width: 36,
-                  child: RiveAnimation.asset(
-                    "assets/RivAssets/icons.riv",
-                    artboard: "AUDIO",
-                    onInit: (artboard) {
-                      StateMachineController controller =
-                          RiveUtils.getRiveController(artboard,
-                              stateMachineName: "AUDIO_Interactivity");
-                      searchTigger = controller.findSMI("active") as SMIBool;
-                    },
-                  ),
-                ),
-              )
+              ...List.generate(
+                  bottomNavs.length,
+                  (index) => GestureDetector(
+                        onTap: () {
+                          bottomNavs[index].input!.change(true);
+                          if (bottomNavs[index] != selectedBottomNav) {
+                            setState(() {
+                              selectedBottomNav = bottomNavs[index];
+                            });
+                          }
+                          Future.delayed(const Duration(seconds: 1), () {
+                            bottomNavs[index].input!.change(false);
+                          });
+                        },
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            AnimatedBar(
+                                isActive:
+                                    bottomNavs[index] == selectedBottomNav),
+                            SizedBox(
+                              height: 36,
+                              width: 36,
+                              child: Opacity(
+                                opacity: bottomNavs[index] == selectedBottomNav
+                                    ? 1
+                                    : 0.5,
+                                child: RiveAnimation.asset(
+                                  bottomNavs.first.src,
+                                  artboard: bottomNavs[index].artboard,
+                                  onInit: (artboard) {
+                                    StateMachineController controller =
+                                        RiveUtils.getRiveController(artboard,
+                                            stateMachineName: bottomNavs[index]
+                                                .stateMachineName);
+                                    bottomNavs[index].input =
+                                        controller.findSMI("active") as SMIBool;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ))
             ],
           ),
         ),
@@ -54,3 +82,59 @@ class _MyBottomNavState extends State<MyBottomNav> {
   }
 }
 
+class AnimatedBar extends StatelessWidget {
+  const AnimatedBar({
+    super.key,
+    required this.isActive,
+  });
+
+  final bool isActive;
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      margin: const EdgeInsets.only(bottom: 2),
+      height: 4,
+      width: isActive ? 20 : 0,
+      decoration: const BoxDecoration(
+          color: Color(0xFF81B4FF),
+          borderRadius: BorderRadius.all(Radius.circular(12))),
+    );
+  }
+}
+
+class RiveAsset {
+  final String artboard, stateMachineName, title, src;
+  late SMIBool? input;
+
+  RiveAsset(this.src,
+      {required this.artboard,
+      required this.stateMachineName,
+      required this.title,
+      this.input});
+
+  set SetInput(SMIBool status) {
+    input = status;
+  }
+}
+
+List<RiveAsset> bottomNavs = [
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "CHAT", stateMachineName: "CHAT_Interactivity", title: "Chat"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "SEARCH",
+      stateMachineName: "SEARCH_Interactivity",
+      title: "Search"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "TIMER",
+      stateMachineName: "TIMER_Interactivity",
+      title: "Chat"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "BELL",
+      stateMachineName: "BELL_Interactivity",
+      title: "Notification"),
+  RiveAsset("assets/RiveAssets/icons.riv",
+      artboard: "USER",
+      stateMachineName: "USER_Interactivity",
+      title: "Profile"),
+];
